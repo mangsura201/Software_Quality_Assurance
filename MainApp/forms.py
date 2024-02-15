a0887afc91f601e52aa914e0bc1fbb829826d9ce
ShifatiRabbi <sr9986.abc@gmail.com>
2024-02-15 13:57:21 +0600

    Unit testing for some feature and update relavant file

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -50,17 +50,6 @@ class ProfileUpdateForm(forms.ModelForm):
         if commit:
             user.save()
             return super(ProfileUpdateForm, self).save(commit=commit)
-"""
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        exclude = ['user', 'bank_account_no', 'balance']  # Excluding fields from the form
-
-    def __init__(self, *args, **kwargs):
-        super(ProfileUpdateForm, self).__init__(*args, **kwargs)
-        for field_name in self.Meta.exclude:
-            self.fields[field_name].disabled = True  # Disabling excluded fields
-"""
 
 
 class MoneyTransferForm(forms.Form):
