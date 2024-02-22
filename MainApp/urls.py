1177afafd4aa92eed9d6ba29cbbb24d7a09d45d0
sayma352 <rupa.stu2018@juniv.edu>
2024-02-22 10:52:09 +0600

    performed see all customers by sayma

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, see_all_customer_views
 
 urlpatterns = [
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('see_all_users/', views.see_all_users, name='see_all_users'),
+    path('see_all_customers/', see_all_customer_views.see_all_customers, name='see_all_customers'),
     path('see_all_transactions/', views.see_all_transactions, name='see_all_transactions'),
     path('add_new_user/', views.add_new_user, name='add_new_user'),
     path('login_to_user/', views.login_to_user, name='login_to_user'),
