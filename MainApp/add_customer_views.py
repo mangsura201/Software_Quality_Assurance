e952d3aa394d9281029eb9a9acde3d6cab1bc781
mangsura201 <mangsura.stu2018@juniv.edu>
2024-02-22 11:44:26 +0600

    Unit Testing of Add_Customer Performed by Mangsura

--- MainApp/add_customer_views.py
+++ MainApp/add_customer_views.py
@@ -8,6 +8,17 @@ from .forms import *
 
 #@login_required
 def add_customer(request):
+    """
+    View function to add a new customer.
+
+    If the request method is POST, the function validates the form data and saves the new customer.
+    If the form is valid, a success message is displayed, and the user is redirected to the admin dashboard.
+    If the request method is GET, a new instance of the CustomerCreationForm is created.
+
+    :param request: HttpRequest object representing the request made to the server.
+    :return: Rendered HTML template with the form for adding a new customer.
+    """
+    
     if request.method == 'POST':
         form = CustomerCreationForm(request.POST)
         if form.is_valid():
