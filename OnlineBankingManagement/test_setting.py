5b5a4d232d89871a6634e59433515d5ffe6f6852
ShifatiRabbi <sr9986.abc@gmail.com>
2024-03-03 07:04:05 +0600

    Branch Merge conflict solve - 2.1

--- OnlineBankingManagement/test_setting.py
+++ OnlineBankingManagement/test_setting.py
@@ -1,5 +1,4 @@
 from .settings import *
-<<<<<<< HEAD
 DATABASES={
 "default":{
 "ENGINE":"django.db.backends.sqlite3",
@@ -7,14 +6,3 @@ DATABASES={
 }
 } 
 EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'
-=======
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory",
-    }
-}
-
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
->>>>>>> 62615bc1b85e5176a07671444b0786e27d168e66
